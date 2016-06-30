61116d2bdcd4be30e96b2f32d24ed4e1fa75f62b
Ahmed Onawale <ahmed.onawale@andela.com>
2016-06-30 01:03:23 +0100

    Add SwiftDataTests model with Person entity Add SwiftDataTests tests

--- SwiftDataTests/SwiftDataTests.swift
+++ SwiftDataTests/SwiftDataTests.swift
@@ -7,30 +7,100 @@
 //
 
 import XCTest
+import CoreData
 @testable import SwiftData
 
+class Person: NSManagedObject {
+    @NSManaged var name: String
+    @NSManaged var age: Int64
+}
+
+extension Person: KeyCodeable {
+    enum Key: String {
+        case name
+        case age
+    }
+}
+
 class SwiftDataTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        Person.destroyAll()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testEmptyObject() {
+        let person = Person.create()
+        XCTAssertNotNil(person)
+        XCTAssertEqual(person.name, "")
+        XCTAssertEqual(person.age, 0)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testUpdateObject() {
+        let person = Person.create()
+        person.setProperties([.name: "Ahmed", .age: 18])
+        XCTAssertEqual(person.name, "Ahmed")
+        XCTAssertEqual(person.age, 18)
+    }
+
+    func testCreateObjectWithProperties() {
+        let person = Person.create([.name: "Onawale", .age: 20])
+        XCTAssertEqual(person.name, "Onawale")
+        XCTAssertEqual(person.age, 20)
+        let prop = person.getProperties([.name, .age])
+        XCTAssertEqual(prop["name"] as? String, "Onawale")
+        XCTAssertEqual(prop["age"] as? Int, 20)
     }
     
+    func testFindAllObjects() {
+        _ = Person.create([.name: "Ahmed", .age: 10])
+        _ = Person.create([.name: "Onawale", .age: 28])
+        _ = Person.create([.name: "Ayo", .age: 40])
+        let people = Person.findAll()
+        XCTAssertEqual(people.count, 3)
+    }
+    
+    func testFindObjectById() {
+        let id = Person.create([.name: "Ahmed", .age: 29]).objectID
+        let person = Person.findById(id)
+        XCTAssertNotNil(person)
+        XCTAssertEqual(person?.name, "Ahmed")
+        XCTAssertEqual(person?.age, 29)
+    }
+    
+    func testFindObjectByNSURL() {
+        let url = Person.create([.name: "Ahmed", .age: 29]).objectID.URIRepresentation()
+        let person = Person.findByNSURL(url)
+        XCTAssertNotNil(person)
+        XCTAssertEqual(person?.name, "Ahmed")
+        XCTAssertEqual(person?.age, 29)
+    }
+    
+    func testDestroyObject() {
+        let person = Person.create()
+        XCTAssertNotNil(person)
+        person.destroy()
+        XCTAssertNil(Person.findById(person.objectID))
+    }
+    
+    func testDestroyAllObjects() {
+        _ = Person.create([.name: "Ahmed", .age: 10])
+        _ = Person.create([.name: "Onawale", .age: 28])
+        _ = Person.create([.name: "Ayo", .age: 40])
+        XCTAssertEqual(Person.findAll().count, 3)
+        Person.destroyAll()
+        XCTAssertEqual(Person.findAll().count, 0)
+    }
+    
+    func testSaveContext() {
+        XCTAssertFalse(Person.save())
+        let person = Person.create([.name: "Ahmed", .age: 33])
+        XCTAssertTrue(person.save())
+        XCTAssertFalse(person.save())
+        person.setProperties([.name: "Onawale"])
+        XCTAssertTrue(Person.save())
+    }
 }
